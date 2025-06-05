5228d84e4390b24bacc75522ce862b79d602da96
hazemphoenix <hazem.mando408@gmail.com>
2025-06-05 14:48:26 +0300

    install tailwind, angularfire, fontawesome

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { getDatabase, provideDatabase } from "@angular/fire/database";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: "mockrview",
+        appId: "1:417992497103:web:e9909044cb8e7cba41a4ba",
+        storageBucket: "mockrview.firebasestorage.app",
+        apiKey: "AIzaSyBMDhVP5KLjW6WUIYNiW77y7nEkEDiV2Co",
+        authDomain: "mockrview.firebaseapp.com",
+        messagingSenderId: "417992497103",
+        measurementId: "G-BRCQJ56CG1",
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+  ],
 };
