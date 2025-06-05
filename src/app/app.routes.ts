6cf1bbec5673d13e8f3313a938feb3aa2db034cd
Adel Srour <adelmalys@hotmail.com>
2025-06-05 15:43:06 +0300

    Add routes to router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", loadComponent: () => import("./pages/home/home.component").then(p => p.HomeComponent) },
+    {path:"room", loadComponent: ()=> import("./pages/room/room.component").then(p => p.RoomComponent)},
+    {path:"**", loadComponent: ()=> import("./pages/notfound/notfound.component").then(p => p.NotfoundComponent)}
+];
