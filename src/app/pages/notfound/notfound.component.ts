a2e0d51bfe887c4c8b79fdfcddb220c5f25eab2c
hazemphoenix <hazem.mando408@gmail.com>
2025-06-10 15:26:52 +0300

    feat: implement and style the not found page

--- src/app/pages/notfound/notfound.component.ts
+++ src/app/pages/notfound/notfound.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-notfound',
-  imports: [],
-  templateUrl: './notfound.component.html',
-  styleUrl: './notfound.component.scss'
+  selector: "app-notfound",
+  imports: [CommonModule],
+  templateUrl: "./notfound.component.html",
+  styleUrl: "./notfound.component.scss",
 })
 export class NotfoundComponent {
+  constructor(private router: Router) {}
 
+  goHome() {
+    this.router.navigate(["/"]);
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
